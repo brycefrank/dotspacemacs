f8a6941aeb7340ab2f294c5065e6a76991bf60dc
brycefrank <bryce.frank@oregonstate.edu>
2017-11-27 08:01:38 -0800

    Small changes

--- init.el
+++ init.el
@@ -308,7 +308,7 @@ before packages are loaded. If you are unsure, you should try in setting them in
   (if (and (eq 3840 (x-display-pixel-width))
            (eq 2160 (x-display-pixel-height)))
     (setq-default dotspacemacs-default-font '("Source Code Pro" :size 34))
-    (setq-default dotspacemacs-default-font '("Source Code Pro" :size 18)))
+    (setq-default dotspacemacs-default-font '("Source Code Pro" :size 14)))
   )
 
 (defun dotspacemacs/user-config ()
@@ -340,4 +340,4 @@ you should place your code here."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((((class color) (min-colors 89)) (:foreground "#ffffff" :background "#272822")))))
+ '(default ((((class color) (min-colors 89)) ))))
